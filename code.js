017945f626688d41a08fb7cd85231ff906895342
Stuart Robson <stu@alwaystwisted.com>
2013-04-28 21:59:26 +0200

    add smart resize
    taken from here - http://paulirish.com/2009/throttled-smartresize-jquery-event-handler/

--- code.js
+++ code.js
@@ -1,3 +1,34 @@
+// smart resize function from Paul Irish to stop so many damn resize event fires
+
+(function($,sr){
+
+  // debouncing function from John Hann
+  // http://unscriptable.com/index.php/2009/03/20/debouncing-javascript-methods/
+  var debounce = function (func, threshold, execAsap) {
+      var timeout;
+
+      return function debounced () {
+          var obj = this, args = arguments;
+          function delayed () {
+              if (!execAsap)
+                  func.apply(obj, args);
+              timeout = null;
+          };
+
+          if (timeout)
+              clearTimeout(timeout);
+          else if (execAsap)
+              func.apply(obj, args);
+
+          timeout = setTimeout(delayed, threshold || 100);
+      };
+  }
+  // smartresize 
+  jQuery.fn[sr] = function(fn){  return fn ? this.bind('resize', debounce(fn)) : this.trigger(sr); };
+
+})(jQuery,'smartresize');
+
+
 // wait until the page loads
 $(window).load(function() {
   // if the viewport is narrower than 600px (arbitary)
@@ -15,7 +46,7 @@ $(window).load(function() {
     });
   }
   // check for pesky developer resizing
-  $(window).resize(function() {
+  $(window).smartresize(function() {
     // if the viewport is narrower than 600px (arbitary)
     if (document.width < 600) {
       // change the attribute from narrowsrc to src
